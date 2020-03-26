3bdee9f5b6fefb2d8a178728e461bf1bc99ae641
Mehanic385 <58910349+Mehanic385@users.noreply.github.com>
2020-03-26 10:23:40 +0700

    Create InstallData.sh

---
+++ InstallData.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+echo "[•]Installation Data Termux... "
+sleep 35
+
+pkg update && pkg upgrade
+pkg install git -y
+pkg install x11-repo
+pkg install root-repo
+pkg install unstable-repo
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install git -y
+pkg install curl -y
+pkg install wget -y
+pkg install php -y
+pkg install bash -y
+pkg install lolcat -y
+pkg install ruby -y
+pkg install perl -y
+pkg install openssh -y
+pkg install openssl -y
+pkg install ruby && gem install lolcat -y
+pkg install mc -y
+pkg install neofetch -y
+pkg install screenfetch -y
+pkg install figlet -y
+pkg install nano -y
+pkg install nano clang -y
+pkg install toilet -y
+pkg install nodejs -y
+pkg install apache2 -y
+echo "[•]Package pip2...."
+sleep 15
+pip2 install --upgrade pip
+pip2 install mechanize
+pip2 install bs4
+pip2 install request
+pip2 install requests
+pip2 install colorama
+pip2 install lolcat
+echo "[•]Github.com response...."
+sleep 10
+git clone https://github.com/saydog/saydog-Framework
+git clone https://github.com/saydog/Metasploit-Helper
+git clone https://github.com/saydog/Trojan-creator
+git clone https://github.com/saydog/Ransomware-creator
+git clone https://github.com/saydog/Virus-droid-creator
+git clone https://github.com/saydog/Venom-injection-tool
+git clone https://github.com/saydog/DARK-KING
+git clone https://github.com/saydog/SAYDOG
+git clone https://github.com/cyweb/hammer
+git clone https://github.com/Mebus/cupp
+git clone https://github.com/LOoLzeC/Evil-create-framework.git ECF
+giy clone https://github.com/BangDanz/DarkSpam
+git clone https://github.com/pashayogi/SETAN
+git clone https://github.com/CiKu370/OSIF
+git clone https://github.com/IlayTamvan/Report.git
+git clone https://github.com/TheMagizz/DarkPremium
+git clone https://github.com/karjok/terkey
+git clone https://github.com/Xi4u7/A-Rat
+git clone https://github.com/medbenali/CyberScan
+
+
+
+echo "[•]Packed From Linux....."
+sleep 15
+pkg install socat
+pkg install wireshark
+pkg install irssi
+pkg install aircrack
+pkg install hydra
+pkg install nmap
+pkg install sqlmap
